253987640357e52e2945f2bf13090c49ed3fa1b7
harrisonliu <746016326@qq.com>
2022-12-21 08:45:54 +0800

    2022年测试

---
+++ src/main/java/com/gz/cn/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.gz.cn;
+
+public class HelloWorld {
+
+    public static void sayHello(){
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
---
+++ src/test/java/com/gz/cn/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.gz.cn;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public  void  helloWorldTest(){
+        HelloWorld.sayHello();
+    }
+
+}
